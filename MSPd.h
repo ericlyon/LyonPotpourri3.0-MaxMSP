7cffdcf01ad240282229dbf28c9105e14671522e
ericlyon <audiodidact@gmail.com>
2017-09-08 12:43:15 -0400

    commit from Xcode

--- MSPd.h
+++ MSPd.h
@@ -7,6 +7,5 @@
 #include "ext_obex.h"
 #include "buffer.h"
 #define t_floatarg double
-
 #define LYONPOTPOURRI_MSG "-{LyonPotpourri 3.0}-"
 #define potpourri_announce(objname)  post("%s\t ( %s )",LYONPOTPOURRI_MSG,objname)
